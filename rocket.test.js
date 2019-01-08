33dd01f4576b97d34ca43193e4538556b84569ab
Daniel Moniz <daniel.moniz@gmail.com>
2019-01-08 15:44:26 -0500

    Add skeleton code for testing in JS.

---
+++ rocket.test.js
@@ -0,0 +1,22 @@
+
+const Rocket = require('./rocket');
+
+describe('Rocket', () => {
+
+  describe('constructor', () => {
+    it('should set default attributes if nothing is passed', () => {
+      // ...
+    });
+
+    it("should set the rocket's name if provided", () => {
+      // ...
+    });
+  });
+
+  describe('liftOff', () => {
+    // ...
+  });
+
+  // ...
+
+});
